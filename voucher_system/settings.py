38b71e65b7d614416b62cee9aa76c49d2d8b9d46
h-nusterdien <mr.h.nusterdien@gmail.com>
2024-01-14 11:14:04 +0200

    feat: Update "INSTALLED_APPS" in "settings.py" by adding the Django project applications

--- voucher_system/settings.py
+++ voucher_system/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api',
+    'voucher_management',
+    'voucher_redemption',
 ]
 
 MIDDLEWARE = [
